e2bbf7a50ef962e4c7e2dddea45e865fcd1cf491
xu18961999959-sketch <xu18961999959@gmail.com>
2026-03-24 17:04:48 +0800

    feat: add password config

---
+++ docs/data/password.js
@@ -0,0 +1,4 @@
+// 访问密码配置
+// 修改密码：将新密码的 SHA-256 哈希值替换下面这行
+// 计算哈希：https://emn178.github.io/online-tools/sha256.html
+var ACCESS_PASSWORD_HASH = "27f651bf23ceeb84506f43c1dd28c239b8d4df15515f6e7619cc67fbb133c28c";
