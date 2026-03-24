c828a65dedb6d4d24a185c68ce10ba39f84e3eb4
xu18961999959-sketch <xu18961999959@gmail.com>
2026-03-24 17:52:28 +0800

    feat: master secret for daily rotating tokens

--- docs/data/password.js
+++ docs/data/password.js
@@ -1,4 +1,4 @@
-// 访问密码配置
-// 修改密码：将新密码的 SHA-256 哈希值替换下面这行
-// 计算哈希：https://emn178.github.io/online-tools/sha256.html
-var ACCESS_PASSWORD_HASH = "27f651bf23ceeb84506f43c1dd28c239b8d4df15515f6e7619cc67fbb133c28c";
+// 访问码系统配置
+// ACCESS_MASTER_SECRET：改掉这个值可以立即作废所有已生成的码
+// 不要把这个文件分享给客户
+var ACCESS_MASTER_SECRET = "yancheng_zt2026_secret";
